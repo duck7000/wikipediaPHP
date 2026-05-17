ed9f3d9dc0d59bd3767766dcd22a4198afb893b4
duck7000 <15360048+duck7000@users.noreply.github.com>
2026-05-17 21:39:32 +0200

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+#############################################################################
+# wikipediaPHP                                  ed (github user: duck7000)  #
+# written by ed (github user: duck7000)                                     #
+# ------------------------------------------------------------------------- #
+# This program is free software; you can redistribute and/or modify it      #
+# under the terms of the GNU General Public License (see doc/LICENSE)       #
+#############################################################################
+
+require_once __DIR__ . '/src/Wiki/Config.php';
+require_once __DIR__ . '/src/Wiki/Request.php';
+require_once __DIR__ . '/src/Wiki/Api.php';
+require_once __DIR__ . '/src/Wiki/MdbBase.php';
+require_once __DIR__ . '/src/Wiki/WikiContent.php';
+require_once __DIR__ . '/src/Wiki/PageId.php';
+require_once __DIR__ . '/src/Wiki/Search.php';
