855871293c947d1f928d7f377e5d6611832e7cd4
duck7000 <ed@riethorst.net>
2026-07-20 22:57:31 +0200

    WikiContent class added Award to excluded list

--- src/Wiki/WikiContent.php
+++ src/Wiki/WikiContent.php
@@ -84,6 +84,7 @@ class WikiContent extends MdbBase
                         stripos($section->anchor, "Bibliography") !== false ||
                         stripos($section->anchor, "See_also") !== false ||
                         stripos($section->anchor, "Release_history") !== false ||
+                        stripos($section->anchor, "Award") !== false ||
                         stripos($section->anchor, "Further_reading") !== false
                        )
                     {
